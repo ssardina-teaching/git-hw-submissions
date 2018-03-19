ec42923128b973ce408685c7415f01bc8aa53a94
Sebastian Sardina <ssardina@gmail.com>
2018-03-20 10:46:22 +1100

    First version of scrape tool

--- scrape-github.py
+++ scrape-github.py
@@ -1,4 +1,8 @@
-from github import Github
+from github import Github   # package pip3 install pygithub --user
+import csv
+
+
+csvData = [["repo_name", "login", "email"]]
 
 # using username and password
 # g = Github("ssardina@gmail.com", "se22pa30na16")
@@ -10,13 +14,30 @@ g = Github("268de4e6bf5d4ba8241591643a58de2b7512fded")
 #     print(repo.name)
 #     # repo.edit(has_wiki=False)
 
-query = 'pacman berkeley'
-results = g.search_repositories(query)
-for page in results.get_page(1):
-    repo = g.get_repo(page.full_name)
-    print("%s, %s" % (repo.full_name, repo.owner))
-	# print("%s, %s" % (repo.name, repo.user))
+myFile = open('github-repos.csv', 'w')
+with myFile:
+    writer = csv.writer(myFile, delimiter=',')
 
+    query = 'pacman berkeley'
+    results = g.search_repositories(query)
+    # print(results. .totalCount)
+    # exit(1)
+    # for page in results.get_page(0):  # but how do I know how many pages are out there?
+    for page in results:
+        repo = g.get_repo(page.full_name)
+
+        full_name = repo.full_name
+        owner = repo.owner
+        login = owner.login
+        email = owner.email
+
+        print("%s, %s, %s" % (full_name, login, email))
+        # print("%s, %s" % (repo.name, repo.user))
+        csvData.append([full_name, login, email])
+        writer.writerow([full_name, login, email])
+
+# Now write data collected in console
+print(csvData)
 
 
 
