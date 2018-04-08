c86a0929c5b88cd9d5f612493a7f82603d700484
Sebastian Sardina <ssardina@gmail.com>
2018-04-08 11:25:57 +1000

    Improvements to maniupulate repos with gitpython

--- git-assignment-submissions.py
+++ git-assignment-submissions.py
@@ -10,10 +10,17 @@ import os
 import argparse
 import csv
 import logging
-from git import Repo, Git # http://gitpython.readthedocs.io/en/stable/tutorial.html
+import git
+import iso8601
+from pytz import timezone
+import time
+# from git import Repo, Git # http://gitpython.readthedocs.io/en/stable/tutorial.html
 
 # logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.DEBUG, datefmt='%a, %d %b %Y %H:%M:%S')
 logging.basicConfig(format='%(asctime)s %(levelname)-8s %(message)s', level=logging.DEBUG, datefmt='%a, %d %b %Y %H:%M:%S')
+
+DATE_FORMAT = '%-d/%-m/%Y %-H:%-M:%-S'  # RMIT Uni (Australia)
+
 
 
 if __name__ == "__main__":
@@ -39,7 +46,7 @@ if __name__ == "__main__":
     repo_file = open(args.repos_file, 'r')
     repo_reader = csv.DictReader(repo_file, delimiter=',')
 
-    logging.info("We will clone the following repositories: \n")
+    logging.info("Start cloning. Found %d team repositories.\n" % len(list(repo_reader)))
     for row in repo_reader:
         print(row['TEAM'], row['URL'])
     print("\n")
@@ -49,9 +56,19 @@ if __name__ == "__main__":
     tag = 'init'
     if not os.path.exists(git_local_dir):
         # https://gitpython.readthedocs.io/en/2.1.9/reference.html
-        Repo.clone_from(git_url, git_local_dir, branch=tag)
+        git.Repo.clone_from(git_url, git_local_dir, branch=tag)
+
     else:
         logging.warning('Repository %s already exists, ignoring...' % git_url)
+        repo = git.Repo(git_local_dir)
+        print(repo.tags)
+        tag_commit = repo.tags[0].commit
+        tag_date = time.localtime(tag_commit.committed_date)
+        print(time.strftime(DATE_FORMAT, tag_date))
+
+        
+
+        # print(iso8601.parse_date(tag_date).astimezone(timezone(TIMEZONE)))
 
     # Now we clone all the repos in github_repos list
     # for repo in git_repos:
