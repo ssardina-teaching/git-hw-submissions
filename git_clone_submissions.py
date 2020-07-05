0a150808edc41e02b2ca49c8545a40bc44cb77b9
Sebastian <ssardina@gmail.com>
2020-07-05 19:30:07 +1000

    Replaced print for logging statemetns

--- git_clone_submissions.py
+++ git_clone_submissions.py
@@ -35,6 +35,7 @@ def get_tag_time(repo, tag_str):
 
     # tag_commit = next((tag.commit for tag in repo.tags if tag.name == tag_str), None)
     if tag is None:
+        logging.error("\t Repo {} does not have tag {}".format(repo, tag_str))
         return None
     else:
         tag_commit = tag.commit
@@ -83,7 +84,7 @@ def get_teams_from_csv(csv_file, team=None):
     return list_teams
 
 
-def clone_team_repos(list_teams):
+def clone_team_repos(list_teams, tag):
     no_teams = len(list_teams)
     list_teams.sort(key=lambda tup: tup['TEAM'].lower())  # sort the list of teams
     logging.info('Database contains {} teams to clone in folder {}/.'.format(no_teams, output_folder))
@@ -101,62 +102,58 @@ def clone_team_repos(list_teams):
         git_local_dir = os.path.join(output_folder, team_name)
 
         if not os.path.exists(git_local_dir):  # if there is NOT already a local repo for the team
-            print('\t Trying to clone NEW team repo from URL {}.'.format(git_url))
+            logging.info('Trying to clone NEW team repo from URL {}.'.format(git_url))
             try:
-                repo = git.Repo.clone_from(git_url, git_local_dir, branch=submission_tag)
+                repo = git.Repo.clone_from(git_url, git_local_dir, branch=tag)
             except git.GitCommandError as e:
                 team_missing.append(team_name)
                 logging.warning('Repo for team {} with tag {} cannot be cloned: {}'.
-                                format(team_name, submission_tag, e.stderr))
+                                format(team_name, tag, e.stderr))
                 continue
             except KeyboardInterrupt:
                 logging.warning('Script terminated via Keyboard Interrupt; finishing...')
                 sys.exit("keyboard interrupted!")
-            submission_time, submission_commit, tagged_time = get_tag_time(repo, submission_tag)
-            print('\t\t Team {} cloned successfully with tag date {}.'.format(team_name, submission_time))
+            submission_time, submission_commit, tagged_time = get_tag_time(repo, tag)
+            logging.info('Team {} cloned successfully with tag date {}.'.format(team_name, submission_time))
             team_new.append(team_name)
         else:  # OK, so there is already a directory for this team in local repo, check if there is an update
             try:
                 # First get the timestamp of the local repository for the team
                 repo = git.Repo(git_local_dir)
-                submission_time_local, _, _ = get_tag_time(repo, submission_tag)
-                if submission_time_local is None:
-                    print('\t No tag {} in the repository, strange as it was already there...'.format(submission_tag))
-                else:
-                    print('\t Existing LOCAL submission for {} dated {}; updating it...'.format(team_name,
+                submission_time_local, _, _ = get_tag_time(repo, tag)
+                logging.info('Existing LOCAL submission for {} dated {}; updating it...'.format(team_name,
                                                                                                 submission_time_local))
 
                 # Next, update the repo to check if there is a new updated submission time for submission tag
                 repo.remote('origin').fetch(tags=True)
-                submission_time, submission_commit, tagged_time = get_tag_time(repo, submission_tag)
-                if submission_time is None:  # submission_tag has been deleted! remove local repo, no more submission
+                submission_time, submission_commit, tagged_time = get_tag_time(repo, tag)
+                if submission_time is None:  # tag has been deleted! remove local repo, no more submission
                     team_missing.append(team_name)
-                    print('\t No tag {} in the repository for team {} anymore; removing it...'.format(submission_tag,
+                    logging.info('No tag {} in the repository for team {} anymore; removing it...'.format(tag,
                                                                                                       team_name))
                     shutil.rmtree(git_local_dir)
                     continue
 
                 # Checkout the repo from server (doesn't matter if there is no update, will stay as is)
-                repo.git.checkout(submission_tag)
+                repo.git.checkout(tag)
 
                 # Now processs timestamp to report new or unchanged repo
                 if submission_time == submission_time_local:
-                    print('\t\t Team {} submission has not changed.'.format(team_name))
+                    logging.info('Team {} submission has not changed.'.format(team_name))
                     team_unchanged.append(team_name)
                 else:
-                    print('\t Team {} updated successfully with new tag date {}'.format(team_name, submission_time))
+                    logging.info('Team {} updated successfully with new tag date {}'.format(team_name, submission_time))
                     team_updated.append(team_name)
             except git.GitCommandError as e:
                 team_missing.append(team_name)
-                logging.warning(
-                    '\t Problem with existing repo for team {}; removing it: {}'.format(team_name, e.stderr))
+                logging.warning('Problem with existing repo for team {}; removing it: {}'.format(team_name, e.stderr))
                 print('\n')
                 shutil.rmtree(git_local_dir)
                 continue
             except KeyboardInterrupt:
                 logging.warning('Script terminated via Keyboard Interrupt; finishing...')
                 sys.exit(1)
-            except:
+            except: # catch-all
                 team_missing.append(team_name)
                 logging.warning('\t Local repo {} is problematic; removing it...'.format(git_local_dir))
                 print(traceback.print_exc())
@@ -205,7 +202,7 @@ if __name__ == "__main__":
 
     # collect all the options given in CLI
     team_csv_file = args.team_csv_file  # csv file with team git repo links
-    submission_tag = args.tag_str  # tag to grab when cloning
+    submission_tag = args.tag_str  # tag to grab when cloning, if any
     output_folder = args.output_folder  # folder where to clone repos
     submission_timestamps_file = args.file_timestamps  # filename to store timestamps
     add_timestamps = args.add_timestamps  # True if we just append to the timestamp file
@@ -218,8 +215,8 @@ if __name__ == "__main__":
     if os.path.exists(submission_timestamps_file):
         shutil.copy(submission_timestamps_file, submission_timestamps_file + '.bak')
 
-    # Perform the actual cloning of all teams in list_teams
-    teams_cloned, team_new, team_updated, team_unchanged, team_missing = clone_team_repos(list_teams)
+    # Perform the ACTUAL CLONING of all teams in list_teams
+    teams_cloned, team_new, team_updated, team_unchanged, team_missing = clone_team_repos(list_teams, submission_tag)
 
     # Write the submission timestamp file
     if add_timestamps:  # we just append to the file
