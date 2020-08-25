debe5a9ef2de4e28fcc919585efb39d802162b4d
Sebastian <ssardina@gmail.com>
2020-08-25 10:04:49 +1000

    Redone handing of individual/team assignments - Fixes #8

--- gh_classroom_collect.py
+++ gh_classroom_collect.py
@@ -2,7 +2,9 @@
 """
 Script to obtain all the repositories from a GitHub Classroom
 
-Uses PyGithub (https://github.com/PyGithub/PyGithub) as API to GitHub
+Uses PyGithub (https://github.com/PyGithub/PyGithub) as API to GitHub:
+
+    python3 -m pip install PyGithub
 
 Some usage help on PyGithub:
     https://www.thepythoncode.com/article/using-github-api-in-python
@@ -30,7 +32,7 @@ from github import Github, Repository, Organization, GithubException
 import logging
 
 CSV_GITHUB_USERNAME="github_username"
-CSV_TEAM_NAME="identifier"
+CSV_GITHUB_IDENTIFIER= "identifier"
 
 logging.basicConfig(format='%(asctime)s %(levelname)-8s %(message)s', level=logging.INFO,
                     datefmt='%a, %d %b %Y %H:%M:%S')
@@ -79,8 +81,8 @@ if __name__ == '__main__':
     parser.add_argument('-u', '--user', help="GitHub username.")
     parser.add_argument('-t', '--token-file', help="File containing GitHub authorization token/password.")
     parser.add_argument('-p', '--password', help="GitHub username's password.")
-    parser.add_argument('-m', '--team-map', help=f"CSV file mapping team/student id ({CSV_TEAM_NAME}) - "
-                                                 f"GitHub user/team ({CSV_GITHUB_USERNAME}).")
+    parser.add_argument('-m', '--student-map', help=f"CSV file mapping repo suffix ({CSV_GITHUB_USERNAME}) to "
+                                                 f"student ids ({CSV_GITHUB_IDENTIFIER}).")
     args = parser.parse_args()
 
     REPO_URL_PATTERN = re.compile(r'^{}/{}-(.*)$'.format(args.ORG_NAME, args.ASSIGNMENT_PREFIX))
@@ -99,16 +101,17 @@ if __name__ == '__main__':
         logging.error("Something wrong happened during GitHub authentication. Check credentials.")
         exit(1)
 
-    # Populate user to team name mapping dictionary if CSV map was provided
-    user_to_team_map = dict()
-    if args.team_map:
-        with open(args.team_map, 'r') as file:
+    # If --student-map is given, then it is an individual assignment and a mapping should be given
+    # with columns identifier and github_username (the suffix of repos)
+    user_to_id_map = dict()
+    if args.student_map:
+        with open(args.student_map, 'r') as file:
             csv_content = csv.DictReader(file)
             for row in csv_content:
                 row = dict(row)
-                user_to_team_map[row[CSV_GITHUB_USERNAME]] = row[CSV_TEAM_NAME]
+                user_to_id_map[row[CSV_GITHUB_USERNAME]] = row[CSV_GITHUB_IDENTIFIER]
     else:
-        logging.info('No GitHub to Team name mapping provided. Using username as team names.')
+        logging.info('No GitHub individual mapping provided. Team assignment; using suffix repo as identifier.')
 
 
     logging.info(
@@ -127,26 +130,33 @@ if __name__ == '__main__':
         traceback.print_exc()
         exit(1)
 
+    # collect all repos in the organization with the assignment prefix
     repos_select = []
     for repo in org_repos:
         match = re.match(REPO_URL_PATTERN, repo.full_name)
         if match:
             # repo_url = 'git@github.com:{}'.format(repo.full_name)
             logging.info('Found repo {}'.format(repo.full_name))
-            repos_select.append({'USERNAME': match.group(1), 'REPO-NAME': repo.full_name, 'GIT-URL': repo.ssh_url})
+            repos_select.append({'REPO_SUFFIX': match.group(1), 'REPO_NAME': repo.full_name, 'GIT_URL': repo.ssh_url})
 
     # Produce CSV file output with all repos if requested via option --csv
     logging.info('List of repos will be saved to CSV file *{}*.'.format(args.CSV))
     with open(args.CSV, 'w') as output_csv_file:
         csv_writer = csv.DictWriter(output_csv_file,
-                                    fieldnames=['ORG_NAME', 'ASSIGNMENT', 'USERNAME', 'TEAM', 'REPO-NAME', 'GIT-URL'])
+                                    fieldnames=['ORG_NAME', 'ASSIGNMENT', 'REPO_ID', 'REPO_NAME', 'GIT_URL'])
         csv_writer.writeheader()
 
         # for each repo in repo_select produce a row in the CSV file, add the team name from mapping
         for row in repos_select:
-            if row['USERNAME'] in user_to_team_map.keys():
-                row['TEAM'] = user_to_team_map[row['USERNAME']]
+            # if there is a mapping from a repo suffix to a REPO_ID, do it; otherwise use SUFFIX directly
+            if args.student_map:
+                if row['REPO_SUFFIX'] in user_to_id_map.keys():
+                    row['REPO_SUFFIX'] = user_to_id_map[row['REPO_SUFFIX']]
+                else:
+                    logging.warning(
+                        f"Repo {row['REPO_NAME']} with suffix {row['REPO_SUFFIX']} has no mapping. Using suffix directly.")
 
             row['ORG_NAME'] = args.ORG_NAME
             row['ASSIGNMENT'] = args.ASSIGNMENT_PREFIX
+            row['REPO_ID'] = row.pop('REPO_SUFFIX') # rename key REPO_SUFFIX to REPO_ID in the dictionary
             csv_writer.writerow(row)
