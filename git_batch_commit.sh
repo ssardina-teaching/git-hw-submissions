fa58b5231c879040b671ca555f5344e57814461e
ssardina <ssardina@gmail.com>
2022-03-17 23:02:18 +1100

    Better handling of files with spaces; skip non folders

--- git_batch_commit.sh
+++ git_batch_commit.sh
@@ -19,36 +19,52 @@ echo "# parent path -------------->  ${0%/*}  "
 echo "# my name ------------------>  ${0##*/} "
 echo
 
+# change file separator to handle filename with spaces
+# https://www.cyberciti.biz/tips/handling-filenames-with-spaces-in-bash.html
+SAVEIFS=$IFS
+IFS=$(echo -en "\n\b")
 
 #########################
 # HERE GOES THE SCRIPT
 #########################
-for d in $1/*/ ; do
-    echo "=================> Processing $d"
+for dir in $(ls -d $1/*) ; do
 
-    git -C $d pull
+    # continue if not a directory
+  	[ ! -d "$dir" ] && continue
+
+    echo "=================> Processing "$dir""
+
+    git -C "$dir" pull
 
     ######################################################
     # HERE IS WHERE WE DO THE CHANGES TO THE REPO IN $d/
     ######################################################
-    # Get into student repo, add, commit and push 
-    sed -i -e "s/question8/question6/g" $d/analysis.py
-    MESSAGE = "Fixed typo question8() -> question6()"
+    # Get into student repo, add, commit and push
+
+    #sed -i -e "s/question8/question6/g" $d/analysis.py
+
+    cp ../../lp-exercises.git/prolog/prolog-project-agtcity/README.md $dir/
+    MESSAGE="Updated readme spec"
 
     ## Copy good files into student repo
-    # cp pacman-p1-search.git/pacman.py $d/
+    # cp pacman-p1-search.git/pacman.py $dir/
     # MESSAGE = "Updated pacman.py; fix foodEdible issue"
 
     ######################################################
     # FINISH CHANGES
     ######################################################
 
-    git -C $d add .
-    git -C $d commit -m $MESSAGE
-    git -C $d push
+    echo "Will commit with message: **$MESSAGE**"
+    git -C $dir add .
+    git -C $dir commit -m $MESSAGE
+    git -C $dir push
 
     # Get out of student repo (ready to process next)
     echo
 done;
+
+# restore $IFS
+IFS=$SAVEIFS
+
 
 
