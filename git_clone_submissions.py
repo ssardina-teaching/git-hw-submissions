7d0eb5e82add0bf9690c17ea4d71b81aff8da1ac
ssardina <ssardina@gmail.com>
2023-05-17 06:11:46 +0000

    improve counting of commits

--- git_clone_submissions.py
+++ git_clone_submissions.py
@@ -224,7 +224,10 @@ def clone_team_repos(list_repos, tag_str, output_folder):
                 continue
 
         # this just calls git rev-list --count /tags/<tag> to get the number of commits tracing to the tag
-        no_commits = repo.git.rev_list('--count', f"tags/{tag_str}")  # get the no of commits tracing to the tag
+        try:
+            no_commits = repo.git.rev_list('--count', tag_str)  # get the no of commits tracing to the tag
+        except git.exc.GitCommandError:
+            no_commits = repo.git.rev_list('--count', f"tags/{tag_str}")  # get the no of commits tracing to the tag
         repo.close()
         # Finally, write teams that have repos (new/updated/unchanged) into submission timestamp file
         teams_cloned.append(
