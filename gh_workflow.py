f16b7b9645681dd6d1823b2188e727cfabfaa853
ssardina <ssardina@gmail.com>
2025-04-08 23:11:52 +0000

    add until date to start workflow

--- gh_workflow.py
+++ gh_workflow.py
@@ -14,33 +14,28 @@ Some usage help on PyGithub:
 """
 __author__ = "Sebastian Sardina - ssardina - ssardina@gmail.com"
 __copyright__ = "Copyright 2024-2025"
-
 import csv
 import os
-
 from argparse import ArgumentParser
 import util
 
 # https://pygithub.readthedocs.io/en/latest/introduction.html
 from github import Github, Repository, Organization, GithubException
 
+# get the TIMEZONE to be used - ZoneInfo requires Python 3.9+
+TIMEZONE_STR = "Australia/Melbourne"    # https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
+from datetime import datetime
+from zoneinfo import ZoneInfo # Python 3.9+
+TIMEZONE = ZoneInfo(TIMEZONE_STR)
+UTC = ZoneInfo("UTC")
+
+
 import logging
 import coloredlogs
-
-# get the TIMEZONE to be used - works with Python < 3.9 via pytz and 3.9 via ZoneInfo
-TIMEZONE_STR = "Australia/Melbourne"
-from datetime import datetime
-
-# this should work Python 3.9+
-from zoneinfo import ZoneInfo
-
-TIMEZONE = ZoneInfo(TIMEZONE_STR)
-
-
 LOGGING_FMT = "%(asctime)s %(levelname)-8s %(message)s"
 LOGGING_DATE = "%a, %d %b %Y %H:%M:%S"
 LOGGING_LEVEL = logging.INFO
-# LOGGING_LEVEL = logging.DEBUG
+LOGGING_LEVEL = logging.DEBUG
 logging.basicConfig(format=LOGGING_FMT, level=LOGGING_LEVEL, datefmt=LOGGING_DATE)
 coloredlogs.install(level=LOGGING_LEVEL, fmt=LOGGING_FMT, datefmt=LOGGING_DATE)
 
@@ -63,7 +58,9 @@ if __name__ == "__main__":
     parser = ArgumentParser(description="Start automarking workflows")
     parser.add_argument("REPO_CSV", help="List of repositories to get data from.")
     parser.add_argument(
-        "--repos", nargs="+", help="if given, only the teams specified will be parsed."
+        "--repos", 
+        nargs="+", 
+        help="if given, only the teams specified will be parsed."
     )
     parser.add_argument(
         "-t",
@@ -74,6 +71,8 @@ if __name__ == "__main__":
     parser.add_argument("--commit",
                         default="main",
                         help="commit or branch to execute it on %(default)s.")
+    parser.add_argument("--until", 
+                        help="Last commit before this date. Datetime in ISO format, e.g., 2025-04-09T15:30. Overrides --commit.")
     args = parser.parse_args()
 
     now = datetime.now(TIMEZONE).isoformat()
@@ -91,6 +90,7 @@ if __name__ == "__main__":
             f'No repos found in the mapping file "{args.REPO_CSV}". Stopping.'
         )
         exit(0)
+        
 
     ###############################################
     # Authenticate to GitHub
@@ -109,6 +109,15 @@ if __name__ == "__main__":
     ###############################################
     # Process each repo in list_repos
     ###############################################
+    until_dt = None
+    if args.until is not None:
+        until_dt = datetime.fromisoformat(args.until)
+        print(until_dt.tzinfo)
+        if until_dt.tzinfo is None:
+            until_dt = until_dt.replace(tzinfo=TIMEZONE)
+        logging.info(f"Will run workflow on last commit before date: {until_dt.isoformat()} - UTC: {until_dt.astimezone(UTC).isoformat()}")
+    
+    commit = args.commit
     authors_stats = []
     no_repos = len(list_repos)
     workflows_csv = []
@@ -121,29 +130,42 @@ if __name__ == "__main__":
 
         try:
             repo = g.get_repo(repo_name)
-            workflows = repo.get_workflows()
 
+            # override commit if --until is given: get latest commit before until_dt
+            if until_dt is not None:
+                commits = repo.get_commits(until=until_dt.astimezone(UTC))
+                if commits.totalCount == 0:
+                    logging.info(f"\t No commits found before {until_dt.isoformat()}.")
+                    continue
+                commit = commits[0].sha # last commit before until_dt
+                logging.debug(f"\t Commit SHA to run workflow: {commit} - {commits[0].commit.author.date.astimezone(until_dt.tzinfo).isoformat()}")
+
+            # get all workshops and find the one we are looking for (contains args.name)
+            workflows = repo.get_workflows()
             workflow_selected = None
+            print(workflows)
             for w in workflows:
-                if w.name in args.name:
                     logging.info(
-                        f"\t Found workflow ({w}) - Starting it on commit {args.commit}"
+                        f"\t Found workflow ({w}) - Starting it on commit {commit}"
                     )
                     workflow_selected = w
                     break
 
+            if workflow_selected is None:
+                logging.info(f"\t Workflow *{args.name}* not in {repo_name} - {repo_url}.")
+                continue
+
+            # we found the workflow, now run it on commit sha   !
             result = None
-            if workflow_selected:
-                result = workflow_selected.create_dispatch(args.commit)
+            if workflow_selected is not None:
+                # https://pygithub.readthedocs.io/en/latest/github_objects/Workflow.html
+                result = workflow_selected.create_dispatch(commit)
                 if not result:
                     logging.error(
-                        f"\t Workflow {workflow_selected.name} failed to start."
+                        f"\t Workflow *{workflow_selected.name}* failed to start."
                     )
                     no_errors += 1
             else:
-                logging.info(
-                            f"\t Workflow {w.name} not found in repo {repo_name} - {repo_url}."
-                        )
                 no_errors += 1
             workflows_csv.append([row, repo_name, repo_url, result])
         except GithubException as e:
